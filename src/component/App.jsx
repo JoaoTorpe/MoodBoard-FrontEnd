48e4c9a877357f24ac03d4236e325471674b27cb
JoaoTorpe <victotorpe10@gmail.com>
2023-07-23 16:04:36 -0300

    Created a top bar

---
+++ src/component/App.jsx
@@ -0,0 +1,9 @@
+import  './App.css'
+import { Top } from './TopBar'
+function App(){
+
+return <div className='appContainer' > <Top/> </div>
+
+}
+
+export default App 
---
+++ src/component/TopBar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './TopBar.css'
+
+export function Top(){
+ return <div className="container"> <h1>Name's Mood</h1> </div>
+}
