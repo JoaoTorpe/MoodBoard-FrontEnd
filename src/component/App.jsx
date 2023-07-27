d31b39fbd8a36fb8d452408288b578d89b7d367b
JoaoTorpe <victotorpe10@gmail.com>
2023-07-27 16:43:10 -0300

    Now the form pops and unpops

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -2,10 +2,10 @@ import  './App.css'
 import { Top } from './TopBar'
 import {Section} from './Section'
 import { MoodForm } from './Forms/create-mood'
-import { formPop } from './Forms/create-mood'
+import { pop } from './Forms/create-mood'
 function App(){
 
-return <div className='appContainer' > <Top/> <Section/> <MoodForm/> <button onClick={formPop} >Adicionar</button> </div>
+return <div className='appContainer' > <Top/> <Section/> <MoodForm/> <button onClick={pop} >Adicionar</button> </div>
 
 }
 
--- src/component/Forms/create-mood.jsx
+++ src/component/Forms/create-mood.jsx
@@ -1,31 +1,76 @@
-import React from "react";
-import { useEffect, useState } from 'react';
+import React, { useState } from "react";
 import "./create-mood.css"
 
-export function formPop(){
-document.querySelector('.modal-overlay').classList.add('pop')
+
+function Input(props){
+
+return <div>  
+     <input  placeholder={props.holder}  value={props.value} onChange={ event => props.updateValue(event.target.value)}/>
+</div> 
+}
+
+
+
+
+
+
+
+export function pop(){
+    document.querySelector('.modal-overlay').classList.add('pop')
+    
 
 }
 
-function submited(){
+function unPop(){
 
     document.querySelector('.modal-overlay').classList.remove('pop')
+
 }
 
+
+
 export function MoodForm(){
+    const [urlImg,setUrl] = useState("")
+    const [Desc,setDesc] = useState("")
 
+    const [state,SetState] = useState(false)
+    
 
+    function submited(){
 
- return   <div className="modal-overlay">
+    
+        const input = document.querySelectorAll('.modal-overlay .modal-body input')
+    
+        if(input[0].value === ""  && input[0].value === ""){
+            alert("Preencha os campos")
+    
+        }
+        else{
+    
+            unPop()
+        }
+    }
+
+    function unPop(){
+
+        document.querySelector('.modal-overlay').classList.remove('pop')
+        setUrl("")
+        setDesc("")
+    }
+
+ return   <div className="modal-overlay  ">
     <div className="modal-body">
         <h2>Insira as informações</h2>
-        <form method="post" className="input-container">
-           <input  type="text" label="URL"  placeholder="Insira a url da imagem"/>
-           <input type="text"  label="desc"  placeholder="Insira uma descrição"/>
+        <form  className="input-container ">
+           <Input holder="Url da imagem" value= {urlImg} updateValue={setUrl} />
+           <Input holder="Descrição" value= {Desc} updateValue={setDesc} />
         </form>
+       
         <button onClick={submited} value="submit" className="btn-secondary">  
         Postar
         </button>
+        <button className="exit" onClick={unPop} >X</button>
+        
     </div>
 </div>
 
