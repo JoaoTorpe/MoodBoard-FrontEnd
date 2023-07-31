65eaa9edd6929fbea8b81cfaf17a8395a7ff6ade
JoaoTorpe <victotorpe10@gmail.com>
2023-07-31 17:42:33 -0300

    Added the trash can icon

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Board.css'
-
+import img from './assents/delete.png'
 
 export function Board(props){
 
-    return <div className='boardContainer' > 
-    <img className='imgSize' alt='imagem do banco'  src={props.url} /> 
-    <p className='desc' >{props.desc}</p>  </div>
 
+    return <div className='boardContainer' > 
+    
+    <img className='imgSize' alt='imagem do banco'  src={props.url} /> 
+    
+    <div className='descContainer' >
+    <img name={props.url}  className='trashCan' src={img} alt='trash'/>
+    <p className='desc' >  {props.desc}</p>  </div>
+</div>
 }
